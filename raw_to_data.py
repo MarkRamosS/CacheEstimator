e9e053dc1dc2e73e7a970067197fe66baf5d8547
MarkRamosS <el18841@mail.ntua.gr>
2023-04-25 13:45:07 -0400

    Full dataset.

--- raw_to_data.py
+++ raw_to_data.py
@@ -8,15 +8,8 @@ for file in os.listdir('raw_data'):
         lines = f.readlines()
         tmp = False
         d = []
-        ctr = 0
         tlt = False
         for line in lines:
-            if tlt:
-                tlt = False
-                print(line.split('(')[1], end="")
-            if line[:len("*** Reached end of trace:")] == "*** Reached end of trace:":
-                ctr += 1
-                tlt = True
             if line[:9] == "LLC TOTAL":
                 o_llc.append([file[:-6], line.split()[3], line.split()[5]])
             if line[:14] == "cpu0_L2C TOTAL":
@@ -32,8 +25,6 @@ for file in os.listdir('raw_data'):
                 continue
             s = line.split()
             d.append(s)
-        if ctr != 1:
-            print(f"Reached EOF: {ctr} times in file {file[:-6]}")
         if len(o_l1d) != len(o_llc) and len(o_l2c) != len(o_llc):
             print(file+" , ")
         if len(d) != 896:
